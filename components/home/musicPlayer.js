527ae0d0201d9b220e1584579243c4f1cbbdc313
Zh-Drea <cd c/>
2019-01-03 17:55:44 +0800

    change

--- components/home/musicPlayer.js
+++ components/home/musicPlayer.js
@@ -5,7 +5,7 @@
  * @desc 音乐播放界面
  */
 import React,{Component} from 'react';
-import {View,Text,Image,StatusBar,StyleSheet,Dimensions,Platform,TextInput,SectionList,FlatList,ScrollView,TouchableOpacity,Animated,TouchableWithoutFeedback,PanResponder} from 'react-native';
+import {View,Text,Image,StatusBar,StyleSheet,Dimensions,Platform,TextInput,SectionList,FlatList,ScrollView,TouchableOpacity,Animated,TouchableWithoutFeedback} from 'react-native';
 
 
 import BaseComponent from '../../Tool/BaseComponent';
@@ -19,41 +19,6 @@ export default class musicPlayer extends BaseComponent{
 			rotateValue: new Animated.Value(340),
 		}
 	}
-
-	componentWillMount(){
-	   this._panResponder = PanResponder.create({
-	     // 要求成为响应者：
-	     onStartShouldSetPanResponder: (evt, gestureState) => true,
-	     onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-	     onMoveShouldSetPanResponder: (evt, gestureState) => true,
-	     onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-	 
-	     onPanResponderGrant: (evt, gestureState) => {
-	       // 开始手势操作。给用户一些视觉反馈，让他们知道发生了什么事情！
-	 		console.log("---------------------------start----------------------------------------");
-	       // gestureState.{x,y} 现在会被设置为0
-	     },
-	     onPanResponderMove: (evt, gestureState) => {
-	       // 最近一次的移动距离为gestureState.move{X,Y}
-	 		console.log("---------------------------move----------------------------------------");
-	       // 从成为响应者开始时的累计手势移动距离为gestureState.d{x,y}
-	     },
-	     onPanResponderTerminationRequest: (evt, gestureState) => true,
-	     onPanResponderRelease: (evt, gestureState) => {
-	       // 用户放开了所有的触摸点，且此时视图已经成为了响应者。
-	       // 一般来说这意味着一个手势操作已经成功完成。
-	       console.log("---------------------------start_end----------------------------------------");
-	     },
-	     onPanResponderTerminate: (evt, gestureState) => {
-	       // 另一个组件已经成为了新的响应者，所以当前手势将被取消。
-	     },
-	     onShouldBlockNativeResponder: (evt, gestureState) => {
-	       // 返回一个布尔值，决定当前组件是否应该阻止原生组件成为JS响应者
-	       // 默认返回true。目前暂时只支持android。
-	       return true;
-	     }
-	   });
-	 }
 			
 	componentDidMount(){
 		let thiz = this;
@@ -108,7 +73,9 @@ export default class musicPlayer extends BaseComponent{
 				</View>
 				
 				{/*唱针和唱盘和下载，收藏区域*/}
-				<View style={{width:BaseComponent.W,height:BaseComponent.W*400/375,backgroundColor:'blue'}} {...this._panResponder.panHandlers}>
+				<View style={{width:BaseComponent.W,height:BaseComponent.W*400/375,backgroundColor:'blue'}} onStartShouldSetResponder={()=>{return true;}} onResponderRelease={(event)=>{
+						console.log("----------------------------event--------------------------------",event.nativeEvent);
+				}}>
 					<View style={{width:BaseComponent.W,height:BaseComponent.W*80/375,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
 						<Animated.Image style={{marginLeft:BaseComponent.W*15/375,marginTop:-BaseComponent.W*10/375,width:BaseComponent.W*50/375,height:'100%', transform:[{rotate:thiz.state.rotateValue.interpolate({inputRange: [340, 360],outputRange: ['340deg', '360deg']})}]}} source={require('../../image/home/ic_needle.png')} resizeMode="cover"/>
 					</View>
