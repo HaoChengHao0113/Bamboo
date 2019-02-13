00efe9cbed9c46e30684290bbc137043b53b3f36
Zh-Drea <cd c/>
2019-02-13 14:51:59 +0800

    change

--- components/home/musicPlayer.js
+++ components/home/musicPlayer.js
@@ -5,7 +5,7 @@
  * @desc 音乐播放界面
  */
 import React,{Component} from 'react';
-import {View,Text,Image,StatusBar,StyleSheet,Dimensions,Platform,TextInput,SectionList,FlatList,ScrollView,TouchableOpacity,Animated,TouchableWithoutFeedback} from 'react-native';
+import {Slider,View,Text,Image,StatusBar,StyleSheet,Dimensions,Platform,TextInput,SectionList,FlatList,ScrollView,TouchableOpacity,Animated,TouchableWithoutFeedback} from 'react-native';
 
 
 import BaseComponent from '../../Tool/BaseComponent';
@@ -43,7 +43,7 @@ export default class musicPlayer extends BaseComponent{
 		this.state.picRotateValue.setValue(0);
 		Animated.timing(thiz.state.picRotateValue,{
 				toValue:1,
-				duration:10000
+				duration:20000
 			}).start(()=>{
 				thiz.rotating();
 			});
@@ -52,11 +52,11 @@ export default class musicPlayer extends BaseComponent{
 	//渲染界面
 	render(){
 		let thiz = this;
-		thiz.rotating();
+		// thiz.rotating();
 		return (
 			<View style={{flex:1,backgroundColor:'#9e9e9e'}}>
 				{/*顶部导航栏*/}
-				<View style={{width:BaseComponent.W,height:BaseComponent.W*70/375,paddingTop:thiz.isIphoneX(30,BaseComponent.SH),flexDirection:'row',alignItems:'center'}}>
+				<View style={{width:BaseComponent.W,height:BaseComponent.W*70/375,paddingTop:thiz.isIphoneX(30,BaseComponent.SH),flexDirection:'row',alignItems:'center',borderBottomWidth:0.5,borderBottomColor:'white'}}>
 					<TouchableOpacity onPress={()=>{
 						thiz.goBack();
 					}}>
@@ -85,14 +85,14 @@ export default class musicPlayer extends BaseComponent{
 				</View>
 				
 				{/*唱针和唱盘和下载，收藏区域*/}
-				<View style={{width:BaseComponent.W,height:BaseComponent.W*400/375,backgroundColor:'blue'}} onStartShouldSetResponder={()=>{return true;}} onResponderRelease={(event)=>{
+				<View style={{width:BaseComponent.W,height:BaseComponent.W*400/375}} onStartShouldSetResponder={()=>{return true;}} onResponderRelease={(event)=>{
 						console.log("----------------------------event--------------------------------",event.nativeEvent);
 				}}>
 					<View style={{width:BaseComponent.W,height:BaseComponent.W*80/375,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
 						<Animated.Image style={{marginLeft:BaseComponent.W*15/375,marginTop:-BaseComponent.W*10/375,width:BaseComponent.W*60/375,height:BaseComponent.W*80/375, transform:[{rotate:thiz.state.rotateValue.interpolate({inputRange: [340, 360],outputRange: ['340deg', '360deg']})}]}} source={require('../../image/home/ic_needle.png')} resizeMode="stretch"/>
 					</View>
 					
-					<View style={{width:BaseComponent.W,height:BaseComponent.W*230/375,backgroundColor:'red',flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+					<View style={{width:BaseComponent.W,height:BaseComponent.W*230/375,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
 						<View style={{width:BaseComponent.W*230/375,height:BaseComponent.W*230/375,backgroundColor:'yellow',borderRadius:BaseComponent.W*115/375,borderColor:'black',justifyContent:'center',alignItems:'center'}}>
 							<View style={{width:BaseComponent.W*210/375,height:BaseComponent.W*210/375,borderRadius:BaseComponent.W*105/375,backgroundColor:'green',justifyContent:'center',alignItems:'center'}}>
 								<Animated.Image style={{width:BaseComponent.W*180/375,height:BaseComponent.W*180/375,borderRadius:BaseComponent.W*90/375,transform:[{rotate:thiz.state.picRotateValue.interpolate({inputRange: [0, 1],outputRange: ['0deg', '360deg']})}]}} source={require('../../image/home/pic1.jpg')}/>
@@ -100,7 +100,38 @@ export default class musicPlayer extends BaseComponent{
 						</View>
 					</View>
 					
+					<View style={{width:'100%',height:BaseComponent.W*90/375,flexDirection:'row',alignItems:'center',justifyContent:'space-around'}}>
+						<Image style={{width:BaseComponent.W*30/375,height:BaseComponent.W*30/375}} source={require('../../image/home/favorite.png')}></Image>
+						<Image style={{width:BaseComponent.W*30/375,height:BaseComponent.W*30/375}} source={require('../../image/home/download.png')}></Image>
+						<Image style={{width:BaseComponent.W*30/375,height:BaseComponent.W*30/375}} source={require('../../image/home/gedan.png')}></Image>
+						<Image style={{width:BaseComponent.W*30/375,height:BaseComponent.W*30/375}} source={require('../../image/home/discuss.png')}></Image>
+						<Image style={{width:BaseComponent.W*40/375,height:BaseComponent.W*30/375}} source={require('../../image/home/shumore.png')}></Image>
+					</View>
 				</View>	
+				
+				{/*播放进度条*/}
+				<View style={{width:'100%',flexDirection:'row',alignItems:'center'}}>
+					<Text style={{marginLeft:BaseComponent.W*10/375}}>00:45</Text>
+					<Slider style={{width:'75%'}}
+                          minimumValue={0}
+                          maximumValue={100}
+                          maximumTrackTintColor="white"
+                          value={50}
+                          onSlidingComplete={(index)=>{
+                                // thiz.refs.player.seek(index);
+                          }}
+                    />  
+                    <Text>00:45</Text>  
+				</View>
+
+				{/*上一曲，下一曲，播放，暂停*/}
+				<View style={{width:'100%',flexDirection:'row',alignItems:'center',marginTop:BaseComponent.W*40/375}}>
+					<Image style={{width:BaseComponent.W*33/375,height:BaseComponent.W*33/375,marginLeft:BaseComponent.W*10/375}} source={require('../../image/home/favorite.png')}></Image>
+					<Image style={{width:BaseComponent.W*33/375,height:BaseComponent.W*33/375,marginLeft:BaseComponent.W*47/375}} source={require('../../image/home/favorite.png')}></Image>
+					<Image style={{width:BaseComponent.W*33/375,height:BaseComponent.W*33/375,marginLeft:BaseComponent.W*47/375}} source={require('../../image/home/favorite.png')}></Image>
+					<Image style={{width:BaseComponent.W*33/375,height:BaseComponent.W*33/375,marginLeft:BaseComponent.W*47/375}} source={require('../../image/home/favorite.png')}></Image>
+					<Image style={{width:BaseComponent.W*33/375,height:BaseComponent.W*33/375,marginLeft:BaseComponent.W*47/375}} source={require('../../image/home/favorite.png')}></Image>
+				</View>
 			</View>
 		)
 	}
